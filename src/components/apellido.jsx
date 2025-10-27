111a0ea2a14da9498340f6cb77f3212d4f31033c
gilary gacha <gacha2804@gmail.com>
2025-10-26 21:25:59 -0500

    subiendo archivo react

---
+++ src/components/apellido.jsx
@@ -0,0 +1,4 @@
+function apellido ({ apellido }) {
+    return <h2>, {apellido}!</h2>;
+}
+export default apellido;
---
+++ src/components/saludo.jsx
@@ -0,0 +1,4 @@
+function Hola ({ nombre }) {
+    return <h1>Hola, {nombre}!</h1>;
+}
+export default Hola;
